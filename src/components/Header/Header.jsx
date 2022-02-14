4a4f0ddc89956061657aaf206af1687104793c33
Davey-M <davey.meuer@gmail.com>
2022-02-14 16:58:41 -0600

    added header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Weekend Challenge To Do App</h1>
+            <img src="" alt="Logo" />
+        </header>
+    )
+}
+
+export default Header;
